e33287b885c73e157402f10f65073ac5c89bd7d0
rhmv <87086212+rhmv@users.noreply.github.com>
2021-10-05 00:18:44 +0400

    Add files via upload

---
+++ test-6/test.js-6/test-6.js
@@ -0,0 +1,66 @@
+// My Swiper 1
+var swiper = new Swiper(".mySwiper_1", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// My Swiper 2
+var swiper = new Swiper(".mySwiper_2", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// My Swiper 3
+var swiper = new Swiper(".mySwiper_3", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+    1440: {
+      slidesPerView: 5,
+      spaceBetween: 0,
+    },
+  },
+});
+
+
